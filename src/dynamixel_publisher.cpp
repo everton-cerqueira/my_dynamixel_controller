49d54f317ca76ead954dd247799b1c0c976c3729
everton-cerqueira <evertonsousacerqueira@gmail.com>
2018-04-10 14:01:35 -0300

    1.4

--- src/dynamixel_publisher.cpp
+++ src/dynamixel_publisher.cpp
@@ -12,26 +12,40 @@
 #define LOAD 3
 
 #define START 0
-#define ONE 1
-#define TWO 2
-#define THREE 3
-#define FOUR 4
-
+#define HALF_PI 1
+#define PI 2
+#define PI_HALF 3
+#define TWO_PI 4
 
 void motor_command(void);
+void motor_init(float pos1, float pos2, float pos3, float pos4, float pos5);
 
-float motor_state[4], count = 0, motor_position[5] = {0,1.57,3.14,4.71,6.28}; 
-int Estado = 1;
-bool moving;
+struct Motor{
+ float motor_state[4], count, motor_position[5];
+ int Estado;
+ bool moving;
+}MX28;
 
+void motor_init(float pos1, float pos2, float pos3, float pos4, float pos5){
+ 
+ MX28.Estado = 1; 
+ MX28.count = 0;
+ MX28.motor_position[0] = pos1;
+ MX28.motor_position[1] = pos2;
+ MX28.motor_position[2] = pos3;
+ MX28.motor_position[3] = pos4;
+ MX28.motor_position[4] = pos5; 
+ 
+ return;
+}
 
 void msgCallback(const dynamixel_msgs::JointState::ConstPtr& msg)
 {   
-   motor_state[GOAL_POS] = msg->goal_pos;
-   motor_state[CURRENT_POS] = msg->current_pos;
-   motor_state[ERROR] = msg->error;
-   motor_state[LOAD] = msg->load;
-   moving = msg->is_moving;   
+   MX28.motor_state[GOAL_POS] = msg->goal_pos;
+   MX28.motor_state[CURRENT_POS] = msg->current_pos;
+   MX28.motor_state[ERROR] = msg->error;
+   MX28.motor_state[LOAD] = msg->load;
+   MX28.moving = msg->is_moving;   
 
 
    ROS_INFO("Goal Position = %f", msg->goal_pos);	 
@@ -45,6 +59,9 @@ int main(int argc, char **argv)
 {
   //my_dynamixel_controller::MsgDynamixel msg;
   
+  struct Motor mt; 
+  motor_init(0,1.57,3.14,4.71,6.28);
+
   ros::init(argc, argv, "dynamixel_publisher");	
   ros::init(argc, argv, "dynamixel_subscriber");
     
@@ -79,84 +96,84 @@ void motor_command(void)
   ros::Subscriber dynamixel_subscriber = nh.subscribe("tilt_controller/state", 100, msgCallback);
   
  
- switch(Estado)
+ switch(MX28.Estado)
  {
    case 1:
-    msg.data = motor_position[START]; 
+    msg.data = MX28.motor_position[START]; 
     dynamixel_publisher.publish(msg);
     ROS_INFO("send msg = %f", msg.data); 
-    Estado = 2;
+    MX28.Estado = 2;
    break;
   
     case 2:
-     if(motor_position[START] - motor_state[CURRENT_POS] <= 0.01)
+     if(MX28.motor_position[START] - MX28.motor_state[CURRENT_POS] <= 0.01)
      {
       //Tira foto
-      Estado = 3;
-     }else Estado = 1;
+      MX28.Estado = 3;
+     }else MX28.Estado = 1;
     break;
 
 
    case 3:
-    msg.data = motor_position[ONE]; 
+    msg.data = MX28.motor_position[HALF_PI]; 
     dynamixel_publisher.publish(msg);
     ROS_INFO("send msg = %f", msg.data);
-    Estado= 4;
+    MX28.Estado= 4;
    break;
 
    case 4: 
-    if(motor_position[ONE] - motor_state[CURRENT_POS] <= 0.01) 
+    if(MX28.motor_position[HALF_PI] - MX28.motor_state[CURRENT_POS] <= 0.01) 
       {
       //Tira foto
-      Estado = 5;
-     }else Estado = 3;
+      MX28.Estado = 5;
+     }else MX28.Estado = 3;
    break;
 
   case 5:
-    msg.data = motor_position[TWO]; 
+    msg.data = MX28.motor_position[PI]; 
     dynamixel_publisher.publish(msg);
     ROS_INFO("send msg = %f", msg.data);
-    Estado= 6;
+    MX28.Estado = 6;
   break;
 
   case 6:   
-    if(motor_position[TWO] - motor_state[CURRENT_POS] <= 0.01) 
+    if(MX28.motor_position[PI] - MX28.motor_state[CURRENT_POS] <= 0.01) 
      {
      //Tira foto
-      Estado = 7;
-     }else Estado = 5;
+      MX28.Estado = 7;
+     }else MX28.Estado = 5;
   break;
  
   case 7:
-    msg.data = motor_position[THREE]; 
+    msg.data = MX28.motor_position[PI_HALF]; 
     dynamixel_publisher.publish(msg);
     ROS_INFO("send msg = %f", msg.data);
-    Estado= 8;
+    MX28.Estado = 8;
   break;
 
   case 8:    
-   if(motor_position[THREE] - motor_state[CURRENT_POS] <= 0.01) 
+   if(MX28.motor_position[PI_HALF] - MX28.motor_state[CURRENT_POS] <= 0.01) 
      {
      //Tira foto
-      Estado = 9;
-     }else Estado = 7;
+      MX28.Estado = 9;
+     }else MX28.Estado = 7;
   break;
 
   case 9:
-    msg.data = motor_position[FOUR]; 
+    msg.data = MX28.motor_position[TWO_PI]; 
     dynamixel_publisher.publish(msg);
     ROS_INFO("send msg = %f", msg.data);
-    Estado= 10;
+    MX28.Estado = 10;
   break;
 
   case 10:
-    if(motor_position[FOUR] - motor_state[CURRENT_POS] <= 0.01) 
+    if(MX28.motor_position[TWO_PI] - MX28.motor_state[CURRENT_POS] <= 0.01) 
      {
      //Tira foto
-      Estado = 1;
-     }else Estado = 9;
+      MX28.Estado = 1;
+     }else MX28.Estado = 9;
   break;
- }	
+  }	
 
 } 
 
